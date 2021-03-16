7ea6d4021e272934715c7eb540bccc149e493c4c
Ryan Wise <48581165+ryanjwise@users.noreply.github.com>
2021-03-16 15:13:52 +1100

    BMI class revision post review

--- ruby/bmi/bmi.rb
+++ ruby/bmi/bmi.rb
@@ -1,27 +1,41 @@
-Write your code in here
+# Write your code in here
 puts "Welcome to the BMI Calculator"
 puts "Please enter your weight in Kg"
 weight = gets.strip.to_f 
 puts "please enter your height in m"
 height = gets.strip.to_f
 
-bmi = weight/(height*height)
+bmi = weight / (height ** 2)
 
-puts "Your  BMI is #{bmi}"
-if bmi <= 24.9
-    if bmi < 18.5 
-        puts "You are in the Underweight category"
-    else 
-        puts "You are in the Normal weight category"
-    end
+# Original solution
+# puts "Your  BMI is #{bmi.round(2)}" # Round to two decimal places
+# if bmi <= 24.9
+#     if bmi < 18.5 
+#         puts "You are in the Underweight category"
+#     else 
+#         puts "You are in the Normal weight category"
+#     end
+# end
+# if bmi > 24.9
+#     if bmi > 29.9 
+#         puts "You are in the Obese category"
+#     else 
+#         puts "You are in the Overweight category"
+#     end
+# end
+
+# Ed's Solution
+if bmi < 18.5
+    category = 'Underweight'
+elsif bmi < 24.9
+    category = 'Normal weight'
+elsif bmi < 29.9
+    category = 'Overweight'
+else
+    category = "Obese"
 end
-if bmi > 24.9
-    if bmi > 29.9 
-        puts "You are in the Obese category"
-    else 
-        puts "You are in the Overweight category"
-    end
-end
+
+puts "Your BMI is #{bmi.round(2)} and your category is #{category}"
 
 
 =begin
