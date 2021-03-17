b4b0c6c8bd2164b9eb8958863eae2c5f902ad5d3
Ryan Wise <48581165+ryanjwise@users.noreply.github.com>
2021-03-17 15:07:49 +1100

    refactored to group methods

--- ruby/number_guess/number_guess.rb
+++ ruby/number_guess/number_guess.rb
@@ -5,6 +5,15 @@
 # Win on correct guess
 # Limited guesses
 
+def guesses_left?(num_guesses, max_guesses)
+    num_guesses == max_guesses
+end
+
+def get_input(num_guesses, max_guesses)
+    puts "A random number has been selected between 1 and 100. This is guess #{num_guesses} after this, you will have #{max_guesses-num_guesses} remaining."
+    puts "What is your guess?"
+    gets.to_i
+end
 
 # Generate random number
 number = Random.rand(100)
@@ -18,17 +27,16 @@ continue = true
 while continue == true
     
     # iterates num guesses and checks not reached
-    if num_guesses == max_guesses
-        puts "you have reached your max number of guesses"
-        continue = false
+    if guesses_left?(num_guesses, max_guesses)
+        puts "you have reached your maximum number of guesses"
         exit 0
+    else
+        num_guesses += 1
     end
-    num_guesses += 1
 
     #Ask for answer
-    puts "A random number has been selected between 1 and 100. This is guess #{num_guesses} after this, you will have #{max_guesses-num_guesses} remaining."
-    puts "What is your guess?"
-    answer = gets.to_i
+    answer = get_input(num_guesses, max_guesses)
+
     # Evaluate high or low  or correct and respond
     # puts number # For Debugging
     if answer == number
