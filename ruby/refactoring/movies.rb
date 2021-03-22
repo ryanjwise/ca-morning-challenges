e37ec9c62b41ba60507f34fa7aa9b3a1ba1913cb
Ryan Wise <48581165+ryanjwise@users.noreply.github.com>
2021-03-22 13:30:11 +1100

    refactored to decrease maintainance

--- ruby/refactoring/movies.rb
+++ ruby/refactoring/movies.rb
@@ -1,12 +1,12 @@
-def print_menu
-  puts '1. A New Hope'
-  puts '2. Back to the Future'
-  puts '3. The Matrix'
+def print_menu(array)
+  array.each_with_index do | movie, index |
+    puts "#{index+1}. #{movie[:title]}"
+  end
 end
 
 def get_selection(array)
   loop do
-    print_menu
+    print_menu(array)
     print "Enter your choice (1-#{array.length})"
     movie_number = gets.to_i
     if movie_number <= array.length
@@ -23,31 +23,22 @@ def print_selection(movie)
   
   puts "Released: #{movie[:release_date]}"
   puts "Directed by: #{movie[:director]}"
-  
+    
   puts "Staring:"
-  puts "#{movie[:actor1]} as #{movie[:role1]}"
-  puts "#{movie[:actor2]} as #{movie[:role2]}"
-  puts "#{movie[:actor3]} as #{movie[:role3]}"
+  movie[:actors].each_index do | index |
+    puts "#{movie[:actors][index]} as #{movie[:roles][index]}"
+  end
   puts " "
-  
-  # puts "Staring:"
-  # movie[:actors].each do | index |
-  #   puts "#{movie[:actors][index]} as #{movie[:roles][index]}"
-  # end
-  # puts " "
 end
 
 movies = [
-  {title: "A New Hope", release_date: 1977, director: "George Lucas", actor1: "Mark Hamil", actor2: "Harrison Ford",actor3: "Carrie Fisher", role1: "Luke Skywalker", role2: "Han Solo",role3: "Leia Organa"},
+  {title: "A New Hope", release_date: 1977, director: "George Lucas", actors: ["Mark Hamil", "Harrison Ford", "Carrie Fisher"], roles: ["Luke Skywalker", "Han Solo", "Leia Organa"]},
+
+  {title: "Back to the Future", release_date: 1985, director: "Robert Zemeckis", actors: ["Michael J. Fox", "Christopher Lloyd", "Lea Thompson" ], roles: ["Marty McFly", "Doc", "Lorraine Baines"]},
   
-  {title: "Back to the Future", release_date: 1985, director: "Robert Zemeckis", actor1: "Michael J. Fox", actor2: "Christopher Lloyd",actor3: "Lea Thompson", role1: "Marty McFly", role2: "Doc",role3: "Lorraine Baines"},
-  
-  {title: "The Matrix", release_date: 1999, director: "Lana & Lilly Wachowski", actor1: "Keanu Reeves", actor2: "Laurence Fishburne",actor3: "Carrie-Anne Moss", role1: "Neo", role2: "Morpheus",role3: "Trinity"},
-  
-  # {title: "The Matrix", release_date: 1999, director: "Lana & Lilly Wachowski", actors: ["Keanu Reeves", "Laurence Fishburne", "Carrie-Anne Moss"], roles: ["Neo", "Morpheus", "Trinity"]}
+  {title: "The Matrix", release_date: 1999, director: "Lana & Lilly Wachowski", actors: ["Keanu Reeves", "Laurence Fishburne", "Carrie-Anne Moss"], roles: ["Neo", "Morpheus", "Trinity"]}
 ]
 
 movie_number = get_selection(movies)
 print_selection(movies[movie_number-1])
-
 puts 'Goodbye!'
