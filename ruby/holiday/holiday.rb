62fbcf6ec67953185302e941e2646087ae5fc36b
Ryan Wise <48581165+ryanjwise@users.noreply.github.com>
2021-03-23 09:34:19 +1100

    completed holiday challenge

--- ruby/holiday/holiday.rb
+++ ruby/holiday/holiday.rb
@@ -1,2 +1,31 @@
 class Holiday
+attr_accessor :name, :day, :month
+
+    def initialize(name, day, month)
+        @name = name
+        @day = day
+        @month = month
+    end
+
+    def ordinal
+        # Produce an array of digits, in reverse order
+        arr = @day.digits
+        if arr.first == 1
+             ordinal = 'st'
+        elsif arr.first == 2
+            ordinal = 'nd'
+        elsif arr.first == 3
+            ordinal ='rd'
+        else
+            ordinal = 'th'
+        end
+    end
+
+    def display
+        puts "#{@name} is held on the #{@day.to_s + ordinal} of #{@month}"
+    end
+
+    def in_same_month(other_holiday)
+        other_holiday.month == @month
+    end
 end
