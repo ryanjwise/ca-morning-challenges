a6951beffccb75033153bb6ec9b3b5f2bc6633d4
Ryan Wise <48581165+ryanjwise@users.noreply.github.com>
2021-03-29 10:23:04 +1100

    Solution to scrabble

--- ruby/scrabble/scrabble.rb
+++ ruby/scrabble/scrabble.rb
@@ -1,4 +1,35 @@
-def word_ranking(words)
-  # Your code here!
+def evaluate_word(word)
+  score = 0
+  word.chars.each do |character|
+    score += character.ord - 96 # Convert to ascii value and subtract 96 so 'a' =1
+  end
+  score
 end
 
+def word_ranking(words)
+  word_list = words.split(' ')
+  highscore = { word: '', score: 0 }
+
+  word_list.each do |word|
+    score = evaluate_word(word)
+    if score > highscore[:score]
+      highscore[:word] = word
+      highscore[:score] = score
+    end
+  end
+  highscore[:word]
+end
+
+# puts word_ranking("test when we are here")
+# puts word_ranking("a b c d e f g")
+
+# Working on individual character scoring
+# def eval_scrable(word)
+#   score = 0
+#   word.chars.each do |character|
+#     pp character
+#   end
+#   score
+# end
+
+# eval_scrable('tasty')
