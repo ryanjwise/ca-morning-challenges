d1e00703e986d24cecbacf392b9c4a1ad9f8fe1a
Ryan Wise <48581165+ryanjwise@users.noreply.github.com>
2021-03-30 21:49:22 +1100

    progress on daily challenge

--- ruby/the_wanderer/wanderer.rb
+++ ruby/the_wanderer/wanderer.rb
@@ -1,3 +1,45 @@
-def direction_saver(travelers_path)
-	# A journey of a thousand miles begins with a single step...
+def delete_items(array, opposite, current_index)
+  opposite_index = array.find_index(opposite)
+  array.delete_at(opposite_index)
+  array.delete_at(current_index)
+  array
 end
+
+def direction_saver(travelers_path)
+  # A journey of a thousand miles begins with a single step...
+
+  pp travelers_path
+  puts "---"
+  travelers_path.each_with_index do |direction, index|
+    case direction
+    when 'NORTH'
+      opposite = 'SOUTH'
+      if travelers_path.include?(opposite)
+        delete_items(travelers_path, opposite, index)
+      end
+    when 'EAST'
+      opposite = 'WEST'
+      if travelers_path.include?(opposite)
+        delete_items(travelers_path, opposite, index)
+      end
+    when 'SOUTH'
+      opposite = 'NORTH'
+      if travelers_path.include?(opposite)
+        delete_items(travelers_path, opposite, index)
+      end
+    when 'WEST'
+      opposite = 'EAST'
+      if travelers_path.include?(opposite)
+        delete_items(travelers_path, opposite, index)
+      end
+    end
+    puts "---"
+  end
+  travelers_path
+end
+
+pp direction_saver(['NORTH', 'SOUTH', 'SOUTH', 'WEST'])
+puts
+puts "-------------------"
+puts
+pp direction_saver(['NORTH', 'SOUTH', 'EAST', 'WEST', 'NORTH', 'NORTH', 'SOUTH', 'EAST', 'WEST', 'NORTH'])
