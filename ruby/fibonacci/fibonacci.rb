65b90625d20edaf4fa06f8a6ce9134da4e49fcc0
Ryan Wise <48581165+ryanjwise@users.noreply.github.com>
2021-03-31 10:18:26 +1100

    Completed fibonaccii challenge

--- ruby/fibonacci/fibonacci.rb
+++ ruby/fibonacci/fibonacci.rb
@@ -1,3 +1,27 @@
+class NonPositiveNumberError < StandardError
+  def initialize(msg = "Number is not positive")
+    super()
+  end
+end
+
 def fibonacci(count)
   # Start counting...
+  arr = [0, 1]
+  raise NonPositiveNumberError unless count.is_a?(Numeric) && count.positive?
+  if count <= arr.length
+    return arr[count]
+  else
+    (count-1).times { arr << next_sequence(arr) }
+  end
+  return arr.last
 end
+
+def next_sequence(arr)
+  # pp arr # Toggle on to see array build process
+  last = arr[-1]
+  second_last = arr[-2]
+  last + second_last
+end
+
+# puts fibonacci(1)
+# puts fibonacci(7)
