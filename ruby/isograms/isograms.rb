b03a83b03f92e9e04446c4445c11493f36669895
Ryan Wise <48581165+ryanjwise@users.noreply.github.com>
2021-04-19 09:23:04 +1000

    add isogram solution

--- ruby/isograms/isograms.rb
+++ ruby/isograms/isograms.rb
@@ -1,3 +1,9 @@
-def isogram?
+def isogram?(word)
+  isogram = true
+  letters = word.downcase.chars
+  letters.each do | letter |
+    return false if letters.count(letter) > 1
+  end
+  isogram
   # Write your code here
 end
