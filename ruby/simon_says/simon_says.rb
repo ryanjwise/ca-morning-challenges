3ef743e4f9e9f11b65a909918ea7e8a9ddbf1b83
Ryan Wise <48581165+ryanjwise@users.noreply.github.com>
2021-04-21 16:03:29 +1000

    Simon Says solution

--- ruby/simon_says/simon_says.rb
+++ ruby/simon_says/simon_says.rb
@@ -1,1 +1,29 @@
-# Write your code here
+def simon_says(array)
+  number = 0
+  array.each do |command|
+    next if simon_says?(command) == false
+
+    task = command.split('Simon says ').pop.split(' ')
+    number = evaluate_task(number, task[0], task.last.to_i)
+  end
+  number
+end
+
+def simon_says?(string)
+  words = string.split(' ')
+  return true if words[0] == 'Simon' && words[1] == 'says'
+
+  false
+end
+
+def evaluate_task(number, operation, mod)
+  case operation
+  when 'add'
+    number += mod
+  when 'subtract'
+    number -= mod
+  when 'multiply'
+    number *= mod
+  end
+  number
+end
