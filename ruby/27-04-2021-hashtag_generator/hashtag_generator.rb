53501ce8d512485d23caadb20b21ccd98bc9bf49
Ryan Wise <48581165+ryanjwise@users.noreply.github.com>
2021-04-27 09:48:32 +1000

    Add solution to hashtag_generator

--- ruby/27-04-2021-hashtag_generator/hashtag_generator.rb
+++ ruby/27-04-2021-hashtag_generator/hashtag_generator.rb
@@ -1,1 +1,18 @@
-# write your code here
+def hashtag_generator(string)
+  return false if string == ''
+
+  array = string.split(' ')
+  array.unshift('#')
+  # array.map! { &:capitalize }
+  array.map! do |word|
+    characters = word.chars
+    characters[0] = characters[0].upcase
+    characters.join
+  end
+  return_string = array.join
+  return false if return_string.length > 140
+
+  return_string
+end
+
+hashtag_generator('test string')
