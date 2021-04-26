4c288508885c9ea35b814c5d3fa15de45313f3fc
Ryan Wise <48581165+ryanjwise@users.noreply.github.com>
2021-04-27 09:08:18 +1000

    add palindrome solution

--- ruby/palindrome/palindrome.rb
+++ ruby/palindrome/palindrome.rb
@@ -1,1 +1,7 @@
  # Write your code here
+def palindrome?(string)
+  string = string.strip.downcase.split(' ').join.gsub(/[[:punct:]]/, '')
+  string == string.reverse
+end
+
+puts palindrome?('Was it a car or a cat I saw?')
