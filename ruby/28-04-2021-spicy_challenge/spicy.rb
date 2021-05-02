28c83ab5db1a67dbc6d04d1691300e6f95c51d50
Ryan Wise <48581165+ryanjwise@users.noreply.github.com>
2021-05-03 09:03:54 +1000

    Solution to spicy challenge

--- ruby/28-04-2021-spicy_challenge/spicy.rb
+++ ruby/28-04-2021-spicy_challenge/spicy.rb
@@ -1,1 +1,16 @@
-#write your code here
+def spicy_food(spice_array, price_array)
+  spicey_price = []
+  boring_price = []
+  spice_array.each_with_index do |spice, i|
+    case spice
+    when 'S'
+      spicey_price << price_array[i].to_f
+    when 'N'
+      price = price_array[i] / 2.to_f
+      spicey_price << price
+      boring_price << price
+    end
+  end
+
+  [spicey_price.sum, boring_price.sum]
+end
