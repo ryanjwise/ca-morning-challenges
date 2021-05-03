002e266cc98aff6fcac27bb046b24b8127a7f19f
Ryan Wise <48581165+ryanjwise@users.noreply.github.com>
2021-05-04 09:02:10 +1000

    Positive dominant solution

--- ruby/03-05-2021-positive_dominant/positive_dominant.rb
+++ ruby/03-05-2021-positive_dominant/positive_dominant.rb
@@ -1,1 +1,12 @@
-#write your code here
+def positive_dominant(array)
+  pos_count = 0
+  neg_count = 0
+  nums = array.keep_if { |item| item.is_a? Numeric }
+  unless array.empty?
+    array.uniq.each do |item|
+      item >= 0 ? pos_count += 1 : neg_count += 1
+    end
+  end
+  puts "There are #{pos_count} positive, and #{neg_count} negatives"
+  pos_count > neg_count
+end
