561da6819931aa629b6852b1700b20d42b137ee5
Ryan Wise <48581165+ryanjwise@users.noreply.github.com>
2021-05-12 09:21:14 +1000

    Merge Array solution

--- ruby/10-05-2021/merge_array.rb
+++ ruby/10-05-2021/merge_array.rb
@@ -1,1 +1,9 @@
-# Write your code here
+def with_our_powers_combined(array1, array2)
+  captain_planet = []
+  length = array1.length >= array2.length ? array1.length : array2.length
+  length.times do |i|
+    captain_planet << array1[i] unless array1.at(i) == nil
+    captain_planet << array2[i] unless array2.at(i) == nil
+  end
+  captain_planet
+end
