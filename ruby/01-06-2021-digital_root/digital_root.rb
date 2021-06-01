11a8a2e26a01dfd2696dd07c1b2e3d4e657c2b36
Ryan Wise <48581165+ryanjwise@users.noreply.github.com>
2021-06-01 10:26:08 +1000

    Add recursive solution to digital_root

--- ruby/01-06-2021-digital_root/digital_root.rb
+++ ruby/01-06-2021-digital_root/digital_root.rb
@@ -1,1 +1,9 @@
 #write your code here
+def digital_root(num)
+  return -1 if num.class != Integer
+  return -1 if num.negative?
+
+  numbers = num.digits.sum
+  numbers = digital_root(numbers) if numbers >= 10
+  numbers
+end
