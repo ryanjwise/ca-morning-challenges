0a744c4a028ea583e08c6a0239078fe091f48d25
Ryan Wise <48581165+ryanjwise@users.noreply.github.com>
2021-06-15 16:01:41 +1000

    js multiply solution not optimised

--- javascript/15-06-2021-MultiplyValue/multiply-value.js
+++ javascript/15-06-2021-MultiplyValue/multiply-value.js
@@ -1,5 +1,20 @@
 function multiplyValue(value, times) {
-	// Your code here
+  if (!Object.is(typeof times, "number")) return null;
+  const valueString = Object.is(typeof value, "string");
+  if (Object.is(typeof value, "number") || valueString) {
+    if (valueString) {
+      let result = [];
+      for (let i = 0; i < times; i++) {
+        result[i] = value;
+      }
+      return result.join("");
+    } else {
+      console.log("here");
+      return value * times;
+    }
+  } else {
+    return null;
+  }
 }
 
-module.exports = {multiplyValue}
+module.exports = { multiplyValue };
