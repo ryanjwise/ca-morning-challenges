03f8554b08b252c3fb09c251590843efb8025b38
Ryan Wise <48581165+ryanjwise@users.noreply.github.com>
2021-06-16 10:45:21 +1000

    add x and o solution

--- javascript/16-06-2021-ExesAndOhs/exes-and-ohs.js
+++ javascript/16-06-2021-ExesAndOhs/exes-and-ohs.js
@@ -1,7 +1,22 @@
 const exesAndOhs = (string) => {
-	// Your code here
-}
+  // Instantiate x & o counters
+  let [x, o] = [0, 0];
+  // Convert string to lowercase.
+  // Convert string to character array
+  const chars = string.toLowerCase().split("");
+  // Iterate through array counting each x and o counters
+  for (let char of chars) {
+    if (char == "x") {
+      x++;
+    } else if (char == "o") {
+      o++;
+    }
+  }
+  // Compare counts
+  // Return true if even else return false
+  return x === o ? true : false;
+};
 
-
+console.log(exesAndOhs("xxxooo"));
 // Leave this line
-module.exports = {exesAndOhs};
+module.exports = { exesAndOhs };
