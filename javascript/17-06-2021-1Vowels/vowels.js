efb50075dce1c42285b5eea5da418f71de18767a
Ryan Wise <48581165+ryanjwise@users.noreply.github.com>
2021-06-21 09:06:21 +1000

    Add vowel and common character solutions

--- javascript/17-06-2021-1Vowels/vowels.js
+++ javascript/17-06-2021-1Vowels/vowels.js
@@ -1,5 +1,9 @@
 function vowels(string) {
-	// Your code here
+	// List vowels for comparison
+	const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+	// Search string
+	// Return Array 
+	return string.split("").filter(letter => vowels.includes(letter))
 }
 
 module.exports = {vowels}
--- javascript/17-06-2021-2CommonCharacters/common-characters.js
+++ javascript/17-06-2021-2CommonCharacters/common-characters.js
@@ -1,5 +1,15 @@
 function commonCharacters(string1, string2) {
-	// Your code here
+  // Your code here
+  const st1Chars = string1.toLowerCase().split("");
+  const st2Chars = string2.toLowerCase().split("");
+
+  // return Array.from(new Set(st1Chars.filter(letter => st2Chars.includes(letter)))).join("").replace(/\s+/, '')
+  return Array.from(
+    new Set(
+      st1Chars.filter((letter) => letter !== " " && st2Chars.includes(letter))
+    )
+  ).join("");
 }
 
-module.exports = {commonCharacters}
+
+module.exports = { commonCharacters };
