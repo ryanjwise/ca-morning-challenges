73a8bfb720ba5009ab7e9dc24f03627685f165ed
Ryan Wise <48581165+ryanjwise@users.noreply.github.com>
2021-06-23 09:07:34 +1000

    add find last index solution

--- javascript/22-06-2021-FindLastIndex/find-last-index.js
+++ javascript/22-06-2021-FindLastIndex/find-last-index.js
@@ -1,5 +1,27 @@
- const findLastIndex = (array, needle) => {
-	// Your code here
-}
+const findLastIndex = (array, needle) => {
+  let location = -1;
+  const searchKey = Object.keys(needle)[0];
+  const searchValue = Object.values(needle)[0];
+  for (let i = array.length - 1; i >= 0; i--) {
+    if (searchKey in array[i]) {
+			if (array[i][searchKey] == searchValue) {
+				location = i
+				break;
+			};
+    }
+  }
+	return location
+};
 
-module.exports = {findLastIndex}
+const users = [
+  { id: 1, name: "Bob", last: "Brown" },
+  { id: 2, name: "Ted", last: "White" },
+  { id: 3, name: "Matthew", last: "McConaughey" },
+  { id: 4, name: "Kanye", last: "West" },
+];
+
+console.log(findLastIndex(users, {
+  name: "Kanye",
+}))
+
+module.exports = { findLastIndex };
