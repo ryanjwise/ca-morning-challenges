c50a56c34d5b77b30894b1d2d6276bb0f96b33b8
Ryan Wise <48581165+ryanjwise@users.noreply.github.com>
2021-06-28 09:13:41 +1000

    add atomic blond solution

--- javascript/23-06-2021-AtomicBlonde/atomic-blonde.js
+++ javascript/23-06-2021-AtomicBlonde/atomic-blonde.js
@@ -1,5 +1,8 @@
 const atomicBlonde = (num) => {
-  // Your code here
+  const digits = Array.from(String(num), Number)
+  const Product = digits.reduce((total, el) => total *= el)
+  const Sum = digits.reduce((total, el) => total += el)
+  return (Product === Sum)
 };
 
 module.exports = { atomicBlonde };
